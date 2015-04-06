e5a31037229c69e6ad5b474b895307d21f587761
Trip Volpe <trip.volpe@gmail.com>
2015-04-06 00:21:33 -0700

    Add simple CP437 -> Unicode converter

---
+++ decode.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE OverloadedStrings #-}
+import System.Environment
+import Data.Char
+import qualified Data.ByteString as B
+
+-- The Dwarf Fortress character set, which is really just codepage 437.
+dfchars :: String
+dfchars = 
+    " ☺☻♥♦♣♠•◘○◙♂♀♪♫☼" ++
+    "►◄↕‼¶§▬↨↑↓→←∟↔▲▼" ++
+    " !\"#$%&'()*+,-./"++
+    "0123456789:;<=>?" ++
+    "@ABCDEFGHIJKLMNO" ++
+    "PQRSTUVWXYZ[\\]^_"++
+    "`abcdefghijklmno" ++
+    "pqrstuvwxyz{|}~⌂" ++
+    "ÇüéâäàåçêëèïîìÄÅ" ++
+    "ÉæÆôöòûùÿÖÜ¢£¥₧ƒ" ++
+    "áíóúñÑªº¿⌐¬½¼¡«»" ++
+    "░▒▓│┤╡╢╖╕╣║╗╝╜╛┐" ++
+    "└┴┬├─┼╞╟╚╔╩╦╠═╬╧" ++
+    "╨╤╥╙╘╒╓╫╪┘┌█▄▌▐▀" ++
+    "αßΓπΣσµτΦΘΩδ∞φε∩" ++
+    "≡±≥≤⌠⌡÷≈°∙·√ⁿ²■ "
+
+dfToUnicode :: Int -> Char
+dfToUnicode = (!!) dfchars
+
+-- A specialized character conversion for partially-encoded raw files.
+-- Rather than parse the raws to figure out which parts are CP437, we
+-- make two complementary and most likely valid assumptions:
+--    1. The CP437 parts do not contain codes 9, 10 or 13.
+--    2. The rest of the file is printable ASCII and codes 9, 10 or 13.
+-- Using this function to decode a DF language raw should safely produce
+-- a correct raw in which the CP437 characters have been unicodified.
+rawToUnicode :: Int -> Char
+rawToUnicode c
+    | c `elem` [9, 10, 13] = chr c
+    | otherwise            = dfToUnicode c
+
+main :: IO ()
+main = do
+    args <- getArgs
+    contents <- B.readFile $ head args
+    putStr $ map (rawToUnicode . fromIntegral) $ B.unpack contents
